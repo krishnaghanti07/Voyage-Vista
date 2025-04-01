aaafb7d84c6447691c0bc382fbba25d93752bbe3
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2025-04-01 20:35:54 +0530

    Build & added a new map-view feature & developed it's basic feature & functionality

---
+++ public/js/map.js
@@ -0,0 +1,14 @@
+
+//   let mapToken = "<%= process.env.MAP_TOKEN %>";
+
+//   let mapToken = mapToken ;
+//   console.log(mapToken);
+
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.2088, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 9 // starting zoom
+});
