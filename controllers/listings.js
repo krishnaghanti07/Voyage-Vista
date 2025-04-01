3179e10b302cc9c159823b451da7e0f51e09e167
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2025-04-01 23:50:38 +0530

    modify & update the map-view feature & improve functionality by adding markers

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   let allListings = await Listing.find({});
@@ -24,6 +27,16 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
+  // console.log(response.body.features[0].geometry);
+  // res.send("Done..!!");
+
   let url = req.file.path;
   let filename = req.file.filename;
   // console.log(url, "...", filename);
@@ -32,8 +45,12 @@ module.exports.createListing = async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   // console.log(req.user);
   newListing.owner = req.user._id;
-  newListing.image = {url, filename};
-  await newListing.save();
+  newListing.image = { url, filename };
+
+  newListing.geometry = response.body.features[0].geometry;
+
+  let savedListing = await newListing.save();
+  console.log(savedListing);
   // console.log(listing);
   req.flash("success", "New Listing Created..!");
   res.redirect("/listings");
@@ -47,8 +64,8 @@ module.exports.renderEditForm = async (req, res) => {
     res.redirect("/listings");
   }
 
-  let originalImageUrl = listing.image.url ;
-  originalImageUrl = originalImageUrl.replace("/upload" , "/upload/w_350");
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_350");
   res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
